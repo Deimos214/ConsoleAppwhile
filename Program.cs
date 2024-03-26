9f0953f8f2f0689ebb8b6464142aa50255070d4b
Usuario <Usuario@CPD03-M02>
2024-03-26 10:30:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+
+            while (cont <= 100) {
+            Console.WriteLine("1" );
+                cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
