c538ef7f49f3b1d022326034aca2fc70e2fd58d5
Usuario <Usuario@CPD03-M17>
2024-04-02 11:16:03 -0300

    folha de pagamento

--- Program.cs
+++ Program.cs
@@ -8,15 +8,71 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            int cont = 1;
+            double contsal = 0;
+            double salario;
 
-            while (cont <= 100) {
-            Console.WriteLine("1" );
-                cont++;
+
+            Console.WriteLine("Deseja iniciar? S/N");
+            string func = Console.ReadLine().ToLower();
+            int cont = 0;
+            while (func == "s")
+            {
+                Console.WriteLine("Qual a forma de contratação?(Assalariado(A), Comissionado(C), Horista(H))");
+                string forma = Console.ReadLine().ToLower();
+                switch (forma)
+                {
+                    case "a":
+                        Console.WriteLine("informe o salário: ");
+                        double sal = double.Parse(Console.ReadLine());
+                        Console.WriteLine("informe o desconto: ");
+                        double desc = double.Parse(Console.ReadLine());
+                        salario = (sal - desc);
+                        Console.WriteLine("O salário é " + salario);
+                        contsal += salario;
+                        cont++;
+                        break;
+                    case "c":
+                        Console.WriteLine("informe a quantidade de produtos: ");
+                        double produto = double.Parse(Console.ReadLine());
+                        Console.WriteLine("informe o valor da comissão: ");
+                        double comiss = double.Parse(Console.ReadLine());
+                        salario = (produto * comiss);
+                        Console.WriteLine("O salário é " + salario);
+                        contsal += salario;
+                        cont++;
+                        break;
+                    case "h":
+                        Console.WriteLine("informe o valor da hora: ");
+                        double valorh = double.Parse(Console.ReadLine());
+                        Console.WriteLine("informe a quantidade de horas trabalhadas: ");
+                        double qtdhora = double.Parse(Console.ReadLine());
+                        salario = (valorh * qtdhora);
+                        Console.WriteLine("O salário é " + salario);
+                        contsal += salario;
+                        cont++;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+
+
+
+                }
+
+                Console.WriteLine("Deseja iniciar? S/N");
+                func = Console.ReadLine().ToLower();
+
+
             }
+            Console.WriteLine("Total folha de pagamento: " + contsal.ToString("C"));
+            Console.WriteLine("Número de funcionários: " + cont);
+            Console.WriteLine("Media salarial:" + (contsal / cont).ToString("C"));
+
             Console.ReadKey();
         }
     }
 }
+
